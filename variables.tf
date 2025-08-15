61c4d1758688cca52b37b28e8ae453e0d03ba11a
naveen kumar <naveen.lingampelli21@gmail.com>
2025-08-15 21:22:21 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z05123311NS9PJF4CG0SG"
+}
+
+variable "zone_name" {
+    default = "devops84.store"
+}
+
+variable "component" {
+
+}
+
+variable "rule_priority" {
+
+}
